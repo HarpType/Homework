25becb155489509fa0635228b79aff4dd4d933bc
Yaroslav Koyokin <ykoekin@gmail.com>
2018-11-28 00:02:11 +0300

    wrapped MyTask into MyThreadPool

--- MyThreadPool/MyThreadPool/IMyTask.cs
+++ MyThreadPool/MyThreadPool/IMyTask.cs
@@ -6,7 +6,7 @@ namespace MyThreadPool
     /// Интерфейс описывает структуру для управления задачей.
     /// </summary>
     /// <typeparam name="TResult">Тип, возвращаемый задачей.</typeparam>
-    interface IMyTask<TResult>
+    public interface IMyTask<TResult>
     {
         /// <summary>
         /// Возвращает true в случае, если задача выполнена.
@@ -23,6 +23,6 @@ namespace MyThreadPool
         /// </summary>
         /// <typeparam name="TNewResult">Тип, возвращаемый новой задачей</typeparam>
         /// <param name="func">Функция, описывающая задачу.</param>
-        MyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult, TNewResult> func);
+        IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult, TNewResult> func);
     }
 }
