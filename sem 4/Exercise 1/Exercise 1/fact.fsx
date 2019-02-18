f28d3f5669e93222d7f2127f4e8c5de6cec05b39
Yaroslav Koyokin <ykoekin@gmail.com>
2019-02-18 22:29:47 +0300

    Создан проект, написан факториал

---
+++ sem 4/Exercise 1/Exercise 1/fact.fsx
@@ -0,0 +1,10 @@
+﻿let fact x = 
+    if x < 0 then
+        0
+    else
+        let rec acc_fact x acc =
+            if x <= 1 then
+                acc
+            else
+                acc_fact (x-1) acc*(x-1)
+        acc_fact x x
