fde13bb67a1a75ca4ec55256b5419a19b03b74cb
Yaroslav Koyokin <ykoekin@gmail.com>
2019-02-18 23:17:54 +0300

    Задача Фибоначчи. Решение 1

---
+++ sem 4/Exercise 1/Exercise 1/fibonacci.fsx
@@ -0,0 +1,9 @@
+﻿let fibonacci n =
+    let rec acc_fibonacci n i acc1 acc2 =
+            if n = i then
+                acc1
+            elif n > 0 then
+                acc_fibonacci n (i+1) acc2 (acc1+acc2)
+            else
+                acc_fibonacci n (i-1) (acc2-acc1) acc1
+    acc_fibonacci n 0 0 1           
