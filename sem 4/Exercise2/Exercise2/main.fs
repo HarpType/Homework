01284adb769f70416e4a2423e3246822edb2f924
Yaroslav Koyokin <ykoekin@gmail.com>
2019-03-02 19:32:49 +0300

    Дописал палиндром. Написал тесты к 1-ой и 2-ой задачам

--- sem 4/Exercise2/Exercise2/main.fs
+++ sem 4/Exercise2/Exercise2/main.fs
@@ -1,7 +1,7 @@
 ﻿(* Блок задачи 1: вхождение заданного числа в список*)
 
-// Функция, возвращающая позицию первого заданного элемента в заданном списке.
-// Если заданного элемента в списке нет, возвращает None.
+/// Функция, возвращающая позицию первого заданного элемента в заданном списке.
+/// Если заданного элемента в списке нет, возвращает None.
 let firstPosInList x list = 
     let rec iterFirstPosInList x list i =
         match list with 
@@ -11,27 +11,14 @@ let firstPosInList x list =
                 Some(i)
             else
                 iterFirstPosInList x list.Tail (i + 1)
-    iterFirstPosInList x list 1
+    iterFirstPosInList x list 0
 
 
 (* Блок задачи 2: проверка на палиндром*)
 
-// Функция проверяет 2 списка на равенство. Выводит true, если списки равны,
-// false в противном случае.
-let rec listsAreEqual (list1:list<_>) (list2:list<_>) =
-    match (list1,list2) with
-    | (h1::t1, h2::t2) -> 
-        if h1 = h2 then
-            listsAreEqual t1 t2
-        else
-            false
-    | ([],h::t) | (h::t,[]) -> false
-    | ([],[]) -> true
-
-
-// Функция, делящая список на 2 части: в первой части находятся n элементов,
-// во второй - оставшиеся элементы.
-let divideList n (list:list<_>) =
+/// Функция, делящая список на 2 части: в первой части находятся отображённые n элементов,
+/// во второй - оставшиеся элементы.
+let reverseElements n (list:list<_>) =
     if n <= 0 then
         ([], list)
     else
@@ -44,13 +31,20 @@ let divideList n (list:list<_>) =
                 | [] -> (accumList, [])
         iterDivideList n list.Tail [list.Head] 1
 
-// Функция проверяет, является ли заданный список палиндромом.
-// Выводит true, если список является палиндромом,
-// false в противном случае.
-let isPalindrome (list:list<_>) = 
-    match divideList (list.Length / 2) list with
+/// Функция проверяет, является ли заданный список палиндромом.
+/// Выводит true, если список является палиндромом,
+/// false в противном случае.
+let isListPalindrome (list:list<_>) = 
+    let listLength = list.Length
+
+    match reverseElements (listLength / 2) list with
     | (list1, list2) ->
-        if (list.Length % 2 = 0) then
-            listsAreEqual list1 list2
+        if (listLength % 2 = 0) then
+            list1 = list2
         else
-            listsAreEqual list1 list2.Tail
+            list1 = list2.Tail
+
+/// Проверяет, является ли заданная строка палиндромом.
+let isPalindrome (str:string) =
+     str.ToLower().ToCharArray() |> Array.toList |> 
+        List.filter(fun c -> c <> ' ') |> isListPalindrome
