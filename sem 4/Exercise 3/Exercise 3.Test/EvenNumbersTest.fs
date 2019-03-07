8efe57210b8fa447b9d17d08bb6be38a730da8c7
Yaroslav Koyokin <ykoekin@gmail.com>
2019-03-08 00:00:54 +0300

    Добавил тесты. Добавил appveyor.yml.

---
+++ sem 4/Exercise 3/Exercise 3.Test/EvenNumbersTest.fs
@@ -0,0 +1,20 @@
+namespace Tests
+
+module EvenNumbers = 
+    open NUnit.Framework
+    open FsUnit
+    open EvenNumbers.CountingEvenNumbers
+
+    /// Подсчитывает количество чётных чисел 3 раза разными способами,
+    /// объединяя результат в кортеж.
+    let countEvenNumThreeTimes list =
+        (countEvenNumWithMap list, countEvenNumWithFilter list,
+            countEvenNumWithFold list)
+
+    [<Test>]
+    let ``[1..10] list contains 5 even numbers`` ()=
+        [1..10] |> countEvenNumThreeTimes |> should equal (5, 5, 5)
+
+    [<Test>]
+    let ``Epmty list contains 0 even numbers`` ()=
+        [] |> countEvenNumThreeTimes |> should equal (0, 0, 0)
--- sem 4/Exercise 3/Exercise 3/EvenNumbers.fs
+++ sem 4/Exercise 3/Exercise 3/EvenNumbers.fs
@@ -2,16 +2,16 @@
 
     /// Функция подсчитывает количество чётных чисел в списке. 
     /// В основе функции - Seq.map.
-    let countingWithMap list =
+    let countEvenNumWithMap list =
         list |> Seq.map(fun x -> ((x % 2) + 1) % 2) |> 
             Seq.fold(fun acc elem -> acc + elem) 0
 
     /// Подсчитывает количество чётных чисел.
     /// В основе - Seq.filter.
-    let countingWithFilter list =
+    let countEvenNumWithFilter list =
         list |> Seq.filter(fun x -> x % 2 = 0) |> Seq.length
 
     /// Подсчитывает количество чётных чисел.
     /// В основе - Seq.fold.
-    let countingWithFold list =
+    let countEvenNumWithFold list =
         list |> Seq.fold(fun acc elem -> acc + ((elem % 2) + 1) % 2) 0
