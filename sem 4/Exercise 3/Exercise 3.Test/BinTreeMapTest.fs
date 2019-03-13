eb55b9daece3c6e5b415ae43e261fc8415bbd851
Yaroslav Koyokin <ykoekin@gmail.com>
2019-03-13 22:51:05 +0300

    Исправил map для бинарных деревьев, написал тесты к задаче.

---
+++ sem 4/Exercise 3/Exercise 3.Test/BinTreeMapTest.fs
@@ -0,0 +1,34 @@
+﻿namespace Tests
+
+module BinTreeMapTest =
+    open NUnit.Framework
+    open FsUnit
+    open BinTree
+
+    let testBinTree1 = Node(5, Node(4, EmptyNode, EmptyNode), Node(6, EmptyNode, EmptyNode))
+    let resultBinTree1 = Node(25, Node(16, EmptyNode, EmptyNode), Node(36, EmptyNode, EmptyNode))
+
+    let testBinTree2 = Node(4, Node(5, 
+                                    Node(6, EmptyNode, EmptyNode), 
+                                    Node(8, EmptyNode, EmptyNode)), 
+                               Node(3, 
+                                    Node(9, EmptyNode, EmptyNode),
+                                    EmptyNode))
+    let resultBinTree2 = Node(0, Node(1, 
+                                        Node(0, EmptyNode, EmptyNode),
+                                        Node(0, EmptyNode, EmptyNode)),
+                                    Node(1,
+                                        Node(1, EmptyNode, EmptyNode),
+                                        EmptyNode))
+
+    [<Test>]
+    let ``Mapping x^2 with EmptyTree equals EmptyTree`` ()=
+        BinTree.map (EmptyNode) (fun(x) -> x * x) |> should equal (EmptyNode:BinTree<int32>)
+
+    [<Test>]
+    let ``Mapping x^2 with binTree`` ()=
+        BinTree.map testBinTree1 (fun x -> x * x) |> should equal resultBinTree1
+
+    [<Test>]
+    let ``Mapping mod with binTree`` ()=
+        BinTree.map testBinTree2 (fun x -> x % 2) |> should equal resultBinTree2
