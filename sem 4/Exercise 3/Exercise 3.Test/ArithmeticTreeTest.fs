85aded20c6486d10f806db882ab094a3583c3536
Yaroslav Koyokin <ykoekin@gmail.com>
2019-03-14 23:53:52 +0300

    Закодил задачу 3. Написал тесты.

---
+++ sem 4/Exercise 3/Exercise 3.Test/ArithmeticTreeTest.fs
@@ -0,0 +1,30 @@
+﻿namespace Tests
+
+module ArithmeticTreeTest =
+    open NUnit.Framework
+    open FsUnit
+    open BinTree
+
+    let testArithmeticTree1 = Node("+", EmptyNode, Node("3", EmptyNode, EmptyNode))
+    let testArithmeticTree2 = Node("+", 
+                                        Node("-", 
+                                            Node("8", EmptyNode, EmptyNode),
+                                            Node("3", EmptyNode, EmptyNode)),
+                                        Node("*", 
+                                            Node("/",
+                                                Node("16", EmptyNode, EmptyNode),
+                                                Node("4", EmptyNode, EmptyNode)),
+                                            Node("2", EmptyNode, EmptyNode)))
+    let testArithmeticTree3 = Node("/", Node("3", EmptyNode, EmptyNode), Node("0", EmptyNode, EmptyNode))
+    
+    [<Test>]
+    let ``Wrong arithmetic tree should return None`` ()=
+        testArithmeticTree1 |> calculateArithmeticTree |> should equal None
+
+    [<Test>]
+    let ``Calculation testArithemticTree2 equals 13`` ()=
+        testArithmeticTree2 |> calculateArithmeticTree |> Option.get |> should equal 13.0
+
+    [<Test>]
+    let ``Dividing float by zero should return infinity`` ()=
+        testArithmeticTree3 |> calculateArithmeticTree |> Option.get |> should equal infinity
--- sem 4/Exercise 3/Exercise 3/BinTree.fs
+++ sem 4/Exercise 3/Exercise 3/BinTree.fs
@@ -5,6 +5,8 @@
         | Node of 'a * BinTree<'a> * BinTree<'a>
         | EmptyNode
 
+    (* Блок задачи 2: map для бинарных деревьев. *)
+
     /// Позволяет применить заданную функцию func ко всем элементам бинарного дерева.
     /// Возвращает новое дерево, каждый элемент которого - результат 
     /// применения заданной функции.
@@ -12,3 +14,23 @@
         match binTree with
         | EmptyNode -> EmptyNode
         | Node(x, l, r) -> Node(func x, map l func, map r func)
+
+    (* Блок задачи 3: вычисление значения по дереву выражения. *)
+
+    /// Функция вычисляет значение по заданному дереву арифметического выражения.
+    let calculateArithmeticTree arithmeticTree =
+        let rec linearizeArithmeticTree arithmeticTree cont = 
+            match arithmeticTree with
+            | Node(x, l, r) ->
+                match x with
+                | "+" -> linearizeArithmeticTree l (fun(a) -> linearizeArithmeticTree r (fun(b) -> cont(a + b)))
+                | "-" -> linearizeArithmeticTree l (fun(a) -> linearizeArithmeticTree r (fun(b) -> cont(a - b)))
+                | "*" -> linearizeArithmeticTree l (fun(a) -> linearizeArithmeticTree r (fun(b) -> cont(a * b)))
+                | "/" -> linearizeArithmeticTree l (fun(a) -> linearizeArithmeticTree r (fun(b) -> cont(a / b)))
+                | _ -> 
+                    try 
+                        cont(x |> float)
+                    with
+                    | :? System.FormatException -> None
+            | EmptyNode -> None
+        linearizeArithmeticTree arithmeticTree (fun(a) -> Some(a))
