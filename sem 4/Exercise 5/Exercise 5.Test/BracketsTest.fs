937c501a69cddd3f44f30aed2e802147efd00579
Koekin Yaroslav <ykoekin@gmail.com>
2019-04-21 22:03:13 +0300

    Подправил первую задачу, добавил реализацию второй, написал тесты на каждую.

---
+++ sem 4/Exercise 5/Exercise 5.Test/BracketsTest.fs
@@ -0,0 +1,42 @@
+﻿namespace Tests
+
+module BracketsTest = 
+    open NUnit.Framework
+    open FsUnit
+    open Brackets 
+
+    let standartIsBracketsStringCorrect = bracketsStringChekerFactory ('(', ')') ('[', ']') ('{', '}')
+    let ``isBracketsStringCorrectOnlyWith()`` = bracketsStringChekerFactory ('(', ')') ('(', ')') ('(', ')')
+    let ``isBracketsStringCorrectOnlyWith(){}`` = bracketsStringChekerFactory ('(', ')') ('{', '}') ('(', ')')
+    let ``isBracketsStringCorrectThreeOpeningOneClosing`` = bracketsStringChekerFactory ('(', ')') ('{', ')') ('[', ')')
+
+    [<Test>]
+    let ``Correct brackets string with [], (), {}`` ()=        
+        "[bl{abla(blab{}()la)}]" |> standartIsBracketsStringCorrect |> should equal true
+
+    [<Test>]
+    let ``Incorrect brackets string with [], (), {}`` ()=
+        "[hello{)()]]" |> standartIsBracketsStringCorrect |> should equal false
+
+    [<Test>]
+    let ``Correct brackets string only with ()`` ()=
+        "{{{{ (((o(*°▽°*)o))) ╰(▔∀▔)╯  (ﾉ´ヮ)ﾉ*: ･ﾟ" |> ``isBracketsStringCorrectOnlyWith()`` |> should equal true
+
+    [<Test>]
+    let ``Incorrect brackets string only with (), {}`` ()=
+        "({}((}" |> ``isBracketsStringCorrectOnlyWith(){}`` |> should equal false
+
+    [<Test>]
+    let ``Correct brackets string with three opening and one closing`` ()=
+        "() [) {) ({[))) [])" |> isBracketsStringCorrectThreeOpeningOneClosing |> should equal true
+    
+    [<Test>]
+    let ``InCorrect brackets string with three opening and one closing`` ()=
+        "() {} []" |> isBracketsStringCorrectThreeOpeningOneClosing |> should equal false
+
+    [<Test>]
+    let failWithTest ()=
+        (fun () -> bracketsStringChekerFactory ('(', '(') ('[', ']') ('{', '}') |> ignore) |> should throw typeof<System.Exception>
+
+
+
--- sem 4/Exercise 5/Exercise 5/Brackets.fs
+++ sem 4/Exercise 5/Exercise 5/Brackets.fs
@@ -1,35 +1,49 @@
 ﻿module Brackets
     
-    /// Возвращает true, если открывающий и закрывающий скобки
-    /// одного типа. false в противном случае.
-    let isPairBrackets openedBracket closedBracket =
-        if (openedBracket = '(') && (closedBracket = ')') then
-            true
-        elif (openedBracket = '[') && (closedBracket = ']') then
-            true
-        elif (openedBracket = '{') && (closedBracket = '}') then
-            true
-        else false
+    /// Возвращает функцию, которая проверяет корректность скобочной последовательности
+    /// введённой строки. Типы скобок задаются вручную.
+    /// oBri -- открывающая скобка типа i.
+    /// сBri -- закрывающая скобка типа i.
+    let bracketsStringChekerFactory (oBr1, cBr1) (oBr2, cBr2) (oBr3:char, cBr3) =
 
-    /// Возвращает true, если строка скобок корректна и false в противном случае.
-    /// Вызывает исключение, если в строке присутствуют посторонние символы.
-    let isCorrectBrackets bracketsString =
-        let rec checkBracketsWithStack (bracketsList:list<char>) (stack:list<char>) =
-            match bracketsList with
-            | h :: t -> 
-                match h with
-                | '(' | '[' | '{' -> checkBracketsWithStack t (h::stack)
-                | ')' | ']' | '}' ->
-                    if isPairBrackets stack.Head h then
-                        checkBracketsWithStack t stack
+        if (oBr1 = cBr1) || (oBr2 = cBr2) || (oBr3 = cBr3) then
+            failwith "Opening and closing brackets must not be equal"
+        
+        /// Возвращает true, если открывающаяся и закрывающаяся скобки
+        /// одного типа. false в противном случае.
+        let isPairBrackets openingBracket closingBracket =
+            if (openingBracket = oBr1) && (closingBracket = cBr1) then
+                true
+            elif (openingBracket = oBr2) && (closingBracket = cBr2) then
+                true
+            elif (openingBracket = oBr3) && (closingBracket = cBr3) then
+                true
+            else false
+
+        /// Возвращает true, если строка скобок корректна и false в противном случае.
+        /// Вызывает исключение, если в строке присутствуют посторонние символы.
+        let isCorrectBracketsString bracketsString =
+            let rec checkBracketsWithStack (bracketsList:list<char>) (stack:list<char>) =
+                match bracketsList with
+                | h :: t -> 
+                    if (h = oBr1) || (h = oBr2) || (h = oBr3) then
+                        checkBracketsWithStack t (h::stack)
+                    elif ((h = cBr1) || (h = cBr2) || (h = cBr3)) then
+                        if not stack.IsEmpty then
+                            if isPairBrackets stack.Head h then
+                                checkBracketsWithStack t stack.Tail
+                            else 
+                                false
+                        else 
+                            false
                     else 
+                        checkBracketsWithStack t stack
+                | [] -> 
+                    if stack.IsEmpty then
+                        true
+                    else
                         false
-                | _ -> failwith "Incorrect brackets string"
-            | [] -> 
-                if stack.IsEmpty then
-                    true
-                else
-                    false
 
-        checkBracketsWithStack (Seq.toList bracketsString) []
+            checkBracketsWithStack (Seq.toList bracketsString) []
 
+        isCorrectBracketsString
