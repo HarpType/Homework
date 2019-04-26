d3aa76205bd096b1fa31a8729453465d1af75333
Koekin Yaroslav <ykoekin@gmail.com>
2019-04-26 18:25:07 +0300

    Реализована задача на последовательности.

---
+++ sem 4/Control2/Control2.Test/SequencesTest.fs
@@ -0,0 +1,14 @@
+﻿module SequencesTest
+
+    open NUnit.Framework
+    open FsUnit
+    open Sequences
+
+    [<Test>]
+    let plusMinusOneSequenceTest ()=
+        plusMinusOneSequence |> Seq.take 5 |> Seq.toList |> should equal [1; -1; 1; -1; 1]
+
+    [<Test>]
+    let superPlusMinusSequenceTest ()=
+        superPlusMinusSequence |> Seq.take 5 |> Seq.toList |> should equal [1; -2; 3; -4; 5]
+
