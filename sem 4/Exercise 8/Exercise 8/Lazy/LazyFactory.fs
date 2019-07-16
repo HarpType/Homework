69d97fb07334520ca54cff0f77dacc7e82dee4c7
Koekin Yaroslav <ykoekin@gmail.com>
2019-07-16 17:39:47 +0300

    Создал ILazy, SingleLazy и LazyFactory

---
+++ sem 4/Exercise 8/Exercise 8/Lazy/LazyFactory.fs
@@ -0,0 +1,11 @@
+﻿namespace Lazy
+
+module LazyFactory =
+
+    // Класс создаёт объекты ленивых вычислений.
+    type LazyFactory =
+
+        // Метод возвращает объект ленивого вычисления, предоставляющий корректную
+        // работу в однопоточном режиме.
+        // supplier -- лямбда-функция, лежащая в основе ленивого вычисления. 
+        static member CreateSingleThreadedLazy (supplier: unit -> 'a) =
---
+++ sem 4/Exercise 8/Exercise 8/Lazy/SingleThreadLazy.fs
@@ -0,0 +1,16 @@
+﻿namespace Lazy
+
+module SingleThreadLazy =
+
+    open ILazy
+
+    // Описывает работу однопоточных ленивых вычислений.
+    // 'a -- тип, возвращаемый ленивым вычислением.
+    // supplier -- лямбда-функция, лежащая в основе ленивого вычисления.
+    type SingleThreadLazy<'a> (supplier: unit -> 'a) =
+
+        let mutable isHasValue = false
+
+        interface ILazy<'a> with
+            member Get () =
+                if isHasValue then
