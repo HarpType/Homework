53d1287c1b81699291fb7896321254ac2433510c
Koekin Yaroslav <ykoekin@gmail.com>
2019-07-17 16:47:59 +0300

    Реализовал безопасные ленивые вычисления.

---
+++ sem 4/Exercise 8/Exercise 8/Lazy/SafeMultipleThreadLazy.fs
@@ -0,0 +1,37 @@
+﻿namespace Lazy
+
+module SafeMultipleThreadLazy =
+
+    open System.Threading
+    open ILazy
+
+    // Описывает работу многопоточных ленивых вычислений.
+    // 'a -- тип, возвращаемый ленивым вычислением.
+    // supplier -- лямбда-функция, лежащая в основе ленивого вычисления.
+    type SafeMultipleThreadLazy<'a> (supplier: unit -> 'a) =
+
+        let monitor = new obj()
+
+        // Если значение ленивого вычисления уже найдено, то равен true,
+        // false в противном случае.
+        let mutable isHasValue = false
+
+        // Хранит информацию о вычислении лямбда-функции.
+        let mutable (optionValue: option<'a>) = None
+
+        interface ILazy<'a> with
+
+            // Реализация метода ленивого вычисления.
+            // Гарантирует корректную работу в многопоточном режиме.
+            member this.Get () =
+                if Volatile.Read(ref isHasValue) |> not then
+                    lock monitor (fun () -> 
+                        if Volatile.Read(ref isHasValue) |> not then
+                            do optionValue <- Some(supplier())
+                            do isHasValue <- true
+                            optionValue.Value
+                        else
+                            optionValue.Value
+                    )
+                else
+                    optionValue.Value
