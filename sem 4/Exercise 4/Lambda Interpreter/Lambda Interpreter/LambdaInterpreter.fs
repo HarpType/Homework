49c31720962c197d19a8979cd5da86e8e074e5af
Koekin Yaroslav <ykoekin@gmail.com>
2019-08-18 18:03:42 +0300

    Описал структуру лямбда-терма. Описал бета-редукцию.

---
+++ sem 4/Exercise 4/Lambda Interpreter/Lambda Interpreter/LambdaInterpreter.fs
@@ -0,0 +1,40 @@
+﻿module LambdaInterpreter
+
+    /// Описывает структуру лямбда-терма
+    type LambdaTerm = 
+        | Variable of char
+        | Application of LambdaTerm * LambdaTerm
+        | Abstraction of char * LambdaTerm
+
+    let betaReduction leftTerm rightTerm =
+        Variable('t')
+
+    /// Определяет, является ли пара термов редэксом.
+    let isRedex leftTerm rightTerm =
+        match leftTerm with
+        | Abstraction(_) -> true
+        | _ -> false
+
+    /// Выполняет бета-редукцию согласно нормальной стратегии
+    let rec normalBetaReductionStep term = 
+        match term with 
+        | Application(leftTerm, rightTerm) -> 
+            if isRedex leftTerm rightTerm then
+                betaReduction leftTerm rightTerm |> Some
+            else
+                let leftStep = normalBetaReductionStep leftTerm
+                match leftStep with
+                | Some(modifiedLeftTerm) -> 
+                    Application(modifiedLeftTerm, rightTerm) |> Some
+                | None -> 
+                    let rightStep = normalBetaReductionStep rightTerm
+                    match rightStep with
+                    | Some(modifiedRightTerm) -> 
+                        Application(leftTerm, modifiedRightTerm) |> Some
+                    | None -> None
+        | Abstraction(variable, term) ->
+            match normalBetaReductionStep term with
+            | Some(modifiedTerm) ->
+                Abstraction(variable, modifiedTerm) |> Some
+            | None -> None
+        | Variable(_) -> None 
