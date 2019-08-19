ef7a2cdc2e233ec06a1e6273eac062a4b83cf6ea
Koekin Yaroslav <ykoekin@gmail.com>
2019-08-19 19:08:46 +0300

    Добавил тестов.

--- sem 4/Exercise 4/Lambda Interpreter/Lambda Interpreter.Test/LambdaInterpreterTest.fs
+++ sem 4/Exercise 4/Lambda Interpreter/Lambda Interpreter.Test/LambdaInterpreterTest.fs
@@ -13,5 +13,31 @@ module LambdaInterpreter =
         simpleLambdaTerm |> normalBetaReductionStep |> Option.get 
                 |> should equal (Application(Variable('z'), Variable('y')))
 
+    [<Test>]
+    let alphaConversionTest () =
+        /// (λx.λy.x y) (λx.x y) -> λa.(λx.x y) a   (с альфа конверсией)
+        let termForAlphaConversion = Application(
+                                        Abstraction('x', Abstraction('y', Application(Variable('x'), Variable('y')))), 
+                                        Abstraction('x', Application(Variable('x'), Variable('y'))))
 
-    
+        termForAlphaConversion |> normalBetaReductionStep |> Option.get 
+                |> should equal (Abstraction('a', Application(Abstraction('x', Application(Variable('x'), Variable('y'))), 
+                                                                Variable('a'))))
+
+    [<Test>]
+    let ``S K K = I`` () =
+        let SKKTerm = Application(
+                        Application(
+                            Abstraction('x', Abstraction('y', Abstraction('z', Application(Application(Variable('x'), Variable('z')), 
+                                                                                        Application(Variable('y'), Variable('z')))))),
+                            Abstraction('x', Abstraction('y', Variable('x')))),
+                        Abstraction('x', Abstraction('y', Variable('x'))))
+        
+        let mutable checkTerm = SKKTerm
+        for _ in 1 .. 4 do
+            checkTerm <- checkTerm |> normalBetaReductionStep |> Option.get
+
+        checkTerm |> should equal (Abstraction('z', Variable('z')))
+
+        /// Нормальная форма уже достигнута, следующий шаг возвращает None.
+        checkTerm |> normalBetaReductionStep |> should equal None
